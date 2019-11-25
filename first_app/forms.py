a83795d4bacae2a6764b2747e77cf8b92b7fe254
Stefan Gouldson <stefangouldson@gmail.com>
2019-11-25 15:06:54 +0000

    update

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from django.core import validators
 
-def check_for_Z(value):
-    if value[0].lower() != 'z':
-        raise forms.ValidationError('NEEDS TO START WITH Z')
+# def check_for_Z(value):
+#     if value[0].lower() != 'z':
+#         raise forms.ValidationError('NEEDS TO START WITH Z')
+#* in name charfiled put 'validators=[check_for_Z]' to make check for z work
 
 class FormName(forms.Form):
-    name = forms.CharField(validators=[check_for_Z])
+    name = forms.CharField()
     email = forms.EmailField()
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[validators.MaxLengthValidator(0)])
