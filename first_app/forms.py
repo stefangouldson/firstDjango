ad58f121d1441679a4bf4fb85afde4f46d442b59
Stefan Gouldson <stefangouldson@gmail.com>
2019-11-26 16:03:41 +0000

    user model form

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.core import validators
+from django.contrib.auth.models import User
+from first_app.models import UserProfileInfo
 
 # def check_for_Z(value):
 #     if value[0].lower() != 'z':
@@ -20,3 +22,18 @@ class FormName(forms.Form):
 
        if email != vmail:
            raise forms.ValidationError('MAKE SURE EMAILS MATCH')
+
+
+# level 5
+
+# class UserForm(forms.ModelForm):
+#     password = forms.CharField(widget=forms.PasswordInput())
+
+#     class Meta():
+#         model = User
+#         fields = ('username','email','password')
+
+# class UserProfileInfo(forms.ModelForm):
+#     class Meta():
+#         model = UserProfileInfo
+#         fields = ('portfolio_site','profile_pics')
--- first_app/models.py
+++ first_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Topic(models.Model):
@@ -21,3 +22,17 @@ class AccessRecord(models.Model):
 
     def __str__(self):
         return str(self.date)
+
+
+# level 5
+
+class UserProfileInfo(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+
+    portfolio_site = models.URLField(blank=True)
+
+    profile_pic = models.ImageField(upload_to='media/profile_pics',blank=True)
+
+    def __str__(self):
+        return self.user.username
